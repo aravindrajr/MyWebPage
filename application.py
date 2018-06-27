c887a50ff199e8ec8b19da41adef57ae9382a768
Aravind Raj <aravindrajr1995@gmail.com>
2018-06-27 08:34:44 +0530

    temp server for krishna

--- application.py
+++ application.py
@@ -1,34 +1,75 @@
-from flask import Flask
-
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+from flask import Flask,  render_template, request
+import json
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+data = [
+    {'name':'aravind','marks':'50'},
+    {'name':'krishna','marks':'60'},
+    {'name':'leela','marks':'70'},
+    {'name':'kamesh','marks':'80'},
+    {'name':'vincy','marks':'90'},
+    {'name':'vyshnavi','marks':'100'},]
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+@application.route('/')
+def index():
+   return render_template("Home.html")
+
+@application.route('/getDetails', methods = ['GET'])
+def getDetails():
+   return json.dumps(data)
+
+@application.route('/getStudent', methods = ['GET'])
+def getDetailsWithParams():
+    name = request.args.get('name')
+    for i in data:
+        if i['name'] == name:
+            return json.dumps(i)
+    return "Name doesnot exist"
+
+@application.route('/putDetails', methods = ['PUT'])
+def putDetails():
+    try:
+        name = request.form['name']
+        marks = request.form['marks']
+        new = {'name' : name,'marks':marks}
+        data.append(new)
+        return "Data sucessfully added"
+    except:
+        return "Someting went wrong </br> body should have name and marks keys passed"
+
+@application.route('/postDetails', methods = ['POST'])
+def postDetails():
+    try:
+        name = request.form['name']
+        marks = request.form['marks']
+        
+        for i in data:
+            if i['name'] == name:
+                i['marks'] = marks
+                return "Successfully updated"
+        return "Entry dosnt exist to update"
+    except:
+        return "Someting went wrong </br> body should have name and marks keys passed"
+
+@application.route('/deleteDetails', methods = ['DELETE'])
+def deleteDetails():
+    try:
+        name = request.form['name']
+        
+        for i in data:
+            if i['name'] == name:
+                data.remove(i)
+                return "Successfully deleted"
+        return "Entry dosnt exist to delete"
+    except:
+        return "Someting went wrong </br> body should have name passed"
+        
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
-    application.run(host='0.0.0.0', port=80, debug=True)
+    application.run(host='127.0.0.1', port=80, debug=True)
